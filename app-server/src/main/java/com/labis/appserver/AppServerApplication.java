c2cbddf1e712a763e5451e0dbcac7f9800f47385
sergio <sergiomartinez_1997@hotmail.com>
2021-04-06 15:01:54 +0200

    connected to postgis

--- app-server/src/main/java/com/labis/appserver/AppServerApplication.java
+++ app-server/src/main/java/com/labis/appserver/AppServerApplication.java
@@ -1,13 +1,30 @@
 package com.labis.appserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class AppServerApplication {
+public class AppServerApplication implements CommandLineRunner {
+
+    @Autowired
+    private JdbcTemplate jdbc;
 
     public static void main(String[] args) {
         SpringApplication.run(AppServerApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        try {
+            System.out.println("RESULTADO-------->");
+            System.out.println(jdbc.queryForObject("select count(*) from spatial_ref_sys",
+                    Long.class));
+            System.out.println("<--------");
+        } catch (Exception e) {
+            System.out.println("-------------When counting: " + e);
+        }
+    }
 }
