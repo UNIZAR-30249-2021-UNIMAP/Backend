ae1a03d48c6b35a60bf61c5b2243e416c9f3bb2f
sergio <sergiomartinez_1997@hotmail.com>
2021-04-12 11:25:49 +0200

    Connected to postgis and correct DB. (and code cleaning)

--- app-server/src/main/java/com/labis/appserver/rabbit/Receiver.java
+++ app-server/src/main/java/com/labis/appserver/rabbit/Receiver.java
@@ -1,8 +1,13 @@
 package com.labis.appserver.rabbit;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class Receiver {
+
+    @Autowired
+    private JdbcTemplate jdbc;
 
     @RabbitListener(queues = "tut.rpc.requests")
     // @SendTo("tut.rpc.replies") used when the
@@ -10,6 +15,16 @@ public class Receiver {
     public String receiveMessage(String message) {
         String msg = "Hello from appserver!";
         System.out.println("Received in 'appserver/Receiver' <" + message + ">");
+
+        try {
+            System.out.println("RESULTADO-------->");
+            System.out.println(jdbc.queryForObject("select count(*) from adap00",
+                    Long.class));
+            System.out.println("<--------");
+        } catch (Exception e) {
+            System.out.println("-------------When counting: " + e);
+        }
+
         return msg;
     }
 
