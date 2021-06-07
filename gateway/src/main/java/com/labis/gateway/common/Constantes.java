7399d828364d08dc8703514a3d0fab9173c02eff
sergio <sergiomartinez_1997@hotmail.com>
2021-06-07 19:51:09 +0200

    Añadidas constantes a gateway

--- gateway/src/main/java/com/labis/gateway/common/Constantes.java
+++ gateway/src/main/java/com/labis/gateway/common/Constantes.java
@@ -1,8 +1,9 @@
-package com.labis.gateway.common;
+package com.labis.appserver.common;
 
 public class Constantes {
+    //Strings de tipo de petición a la API
     public static final String STRING_LOGIN = "/login";
-    public static final String STRING_INCIDENCIA = "/incidencia"; // Get: Todas las incidencias 
+    public static final String STRING_INCIDENCIA = "/incidencia"; // Get: Todas las incidencias
     public static final String STRING_REGISTRO = "/registro";
     public static final String STRING_INCIDENCIA_MANTENIMIENTO = "/incidencia/mantenimiento"; // Get: Incidencias de un empleado Post: finalizar incidencia
     public static final String STRING_INCIDENCIA_ADMIN = "/incidencia/administrador"; // Asignar/rechazar incidencias
@@ -10,4 +11,12 @@ public class Constantes {
     public static final String STRING_ESPACIO = "/espacio"; // Get: info de un espacio Post: reserva espacio
     public static final String STRING_AFORO = "/aforo"; // Get: lista de espacios y su aforo Post: aforo o regla de aforo
     public static final String STRING_ESPACIOS = "/espacios"; // Get: lista de espacios parametrizada
+
+    //Strings de prioridad de una incidencia
+    public static final String STRING_PRIORIDAD_NORMAL =  "normal";
+    public static final String STRING_PRIORIDAD_URGENTE =  "urgente";
+
+    //Strings de tipo de código que puede devolver una petición al cliente
+    public static final String STRING_STATUS_OK = "OK";
+    public static final String STRING_STATUS_ERROR = "OK";
 }
