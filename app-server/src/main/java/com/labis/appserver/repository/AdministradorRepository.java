256a06ddd1095bfa22f477de35359c12309d5ee1
sergio <sergiomartinez_1997@hotmail.com>
2021-09-10 20:13:27 +0200

    eliminados archivos de administrador

--- app-server/src/main/java/com/labis/appserver/repository/AdministradorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.labis.appserver.repository;
-
-import com.labis.appserver.model.Administrador;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AdministradorRepository extends CrudRepository<Administrador, Long> {
-}
