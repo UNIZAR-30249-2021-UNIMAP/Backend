3e4827c82eeec703eeda2a4f25926920516ff482
sergio <sergiomartinez_1997@hotmail.com>
2021-09-11 01:39:05 +0200

    limitados espacios devueltos a 20

--- app-server/src/main/java/com/labis/appserver/service/EspacioService.java
+++ app-server/src/main/java/com/labis/appserver/service/EspacioService.java
@@ -41,15 +41,17 @@ public class EspacioService {
     public List<Espacio>getEspaciosParametrizados(boolean proyector, String edificio, String tipoDeEspacio, Date fechaInicio, Date fechaFin) {
         List<Espacio> espacios = (List<Espacio>) this.espacioRepository.findAll();
         List<Espacio> resultado = new ArrayList<Espacio>();
-        espacios.forEach( espacio -> {
-            if (    (proyector && espacio.getProyectoresActuales() > 0 || !proyector) &&
-                    (!edificio.isEmpty() && espacio.getEdificio().equals(edificio) || edificio.isEmpty()) &&
-                    (!tipoDeEspacio.isEmpty() && espacio.getTipoDeEspacio().toString().equals(tipoDeEspacio) || tipoDeEspacio.isEmpty()) &&
+        Espacio espacio;
+        for (int i = 0; i < espacios.size() && resultado.size() < 20; i++) {
+            espacio = espacios.get(i);
+            if (    (!proyector || espacio.getProyectoresActuales() > 0) &&
+                    (edificio.isEmpty() || espacio.getEdificio().equals(edificio)) &&
+                    (tipoDeEspacio.isEmpty() || espacio.getTipoDeEspacio().toString().equals(tipoDeEspacio)) &&
                     this.estaLibre(fechaInicio, fechaFin, espacio.getReservas())
             ) {
                 resultado.add(espacio);
             }
-        });
+        }
         return resultado;
     }
 
